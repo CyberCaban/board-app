b3189405ce2192ee0abff1f6de8196b3357cf1bb
CyberCaban <andrewgamer63@gmail.com>
2024-11-09 00:21:21 +0300

    nextjs works inside and outside of docker

--- www/next.config.ts
+++ www/next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = () => {
+const nextConfig: NextConfig = async () => {
   const isProd = process.env.NODE_ENV === "production";
-  const api_url = isProd ? process.env.API_BASE_URL : "http://localhost:5000";
+  const api_url = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+
   return {
     redirects: async () => {
       return [
         {
           source: "/api",
-          destination: api_url,
+          destination: `${api_url}/api`,
           permanent: false,
         },
       ];
@@ -22,9 +23,9 @@ const nextConfig: NextConfig = () => {
         {
           source: "/uploads/:path*",
           destination: `${api_url}/uploads/:path*`,
-        }
+        },
       ];
-    }
+    },
   };
 };
 
