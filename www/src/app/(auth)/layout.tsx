1a97c0a16903a3dd2ab6848a660d4c41e92ef4e4
CyberCaban <andrewgamer63@gmail.com>
2024-11-15 17:35:09 +0300

    moved shared tag main into layout

--- www/src/app/(auth)/layout.tsx
+++ www/src/app/(auth)/layout.tsx
@@ -5,5 +5,9 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      {children}
+    </main>
+  );
 }
--- www/src/app/(auth)/register/page.tsx
+++ www/src/app/(auth)/register/page.tsx
@@ -2,8 +2,8 @@ import RegisterForm from "@/components/RegisterForm";
 
 export default function Register() {
     return (
-        <main className="flex min-h-screen flex-col items-center p-24">
+        <>
             <RegisterForm />
-        </main>
+        </>
     );
 }
