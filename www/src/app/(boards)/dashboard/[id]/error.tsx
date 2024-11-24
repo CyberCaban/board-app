4023fc6bbfbc599f28e5a71ddfbc1c70a9fdbb1d
CyberCaban <andrewgamer63@gmail.com>
2024-11-24 11:42:11 +0300

    - excluded .next folder from docker - fixed link in error

--- www/src/app/(boards)/dashboard/[id]/error.tsx
+++ www/src/app/(boards)/dashboard/[id]/error.tsx
@@ -7,7 +7,7 @@ export default function Error({ error }: { error: Error }) {
     <div>
       <h2>Something went wrong!</h2>
       <p>{error.message}</p>
-      <Link href="/boards">Back to boards</Link>
+      <Link href="/board">Back to boards</Link>
     </div>
   );
 }
