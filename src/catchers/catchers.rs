573e26149ce3426c11e11128ada7743889d0978c
Dmitry <andrewgamer63@gmail.com>
2025-01-29 23:43:10 +0300

    422 catcher

--- src/catchers/catchers.rs
+++ src/catchers/catchers.rs
@@ -3,3 +3,8 @@ pub fn not_found(req: &rocket::Request) -> String {
     let uri = req.uri();
     format!("Not Found at {}", uri)
 }
+
+#[catch(422)]
+pub fn unprocessable_entity() -> String {
+    "Failed to parse: Missing Fields".to_string()
+}
--- src/catchers/mount_catchers.rs
+++ src/catchers/mount_catchers.rs
@@ -4,6 +4,6 @@ use super::{catchers, Catcher};
 
 impl Catcher for Rocket<Build> {
     fn mount_catchers(self) -> Self {
-        self.register("/", catchers![catchers::not_found])
+        self.register("/", catchers![catchers::not_found, catchers::unprocessable_entity])
     }
 }
