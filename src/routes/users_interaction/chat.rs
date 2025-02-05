194f4bc0b4bac421996729df90451f76eb8ee962
Dmitry <andrewgamer63@gmail.com>
2025-02-05 11:27:03 +0300

    working websocket chat!!!

--- src/routes/users_interaction/chat.rs
+++ src/routes/users_interaction/chat.rs
@@ -22,71 +22,34 @@ pub async fn events(ws: WebSocket, ws_state: &State<Arc<WsState>>) -> Channel<'s
     ws.channel(move |stream| {
         Box::pin(async move {
             println!("Connected");
-            let (mut sink, mut receiver) = stream.split();
-            let mut is_handshake = true;
+            let (sink, mut receiver) = stream.split();
             
-            tokio::spawn(async move {
-                loop {
-                    select! {
-                        Some(Ok(message)) = receiver.next() => {
-                            match message {
-                                ws::Message::Text(text) => {
-                                    if is_handshake {
-                                        let user_data = match Token::decode_token(text.to_string()) {
-                                            Ok(token) => token.claims.user,
-                                            Err(_) => {
-                                                let _ = sink.close().await;
-                                                break;
-                                            }
-                                        };
-                                        dbg!(&user_data.username);
-                                        ws_state.register(&user_data.id, sink.clone()).await;
-                                        is_handshake = false;
-                                        continue;
-
-                                    }
-                                    // Handle Text message
-                                    println!("Received Text message: {}", text);
-                                    let m: ChatMessage = match serde_json::from_str(&text) {
-                                        Ok(m) => m,
-                                        Err(_) => ChatMessage::default(),
-                                    };
-                                    let res = m.clone();
-                                    let receiver_id = Uuid::parse_str(&m.receiver_id).unwrap();
-                                    let _ = ws_state.send(&receiver_id, WsMessage::Chat(m)).await;
-                                    let _ = sink.send(Message::Text(json!(res).to_string())).await;
-                                }
-
-                                    ws::Message::Close(close_frame) => {
-                                        // Handle Close message
-                                        println!("Received Close message: {:?}", close_frame);
-                                        let close_frame = ws::frame::CloseFrame {
-                                            code: ws::frame::CloseCode::Normal,
-                                            reason: "Client disconected".to_string().into(),
-                                        };
-                                        let _ = sink.close().await;
-                                        break;
-                                    }
-
-                                    _ => {
-                                        println!("Received other message: {:?}", message);
-                                    }
-                                }
-                        }
-                        else => {
-                                println!("Connection closed");
-                                let close_frame = ws::frame::CloseFrame {
-                                    code: ws::frame::CloseCode::Normal,
-                                    reason: "Client disconected".to_string().into(),
-                                };
-                                let _ = sink.close().await;
-                                break;
-                            }
-                    }
+            // Wait for initial handshake
+            while let Some(Ok(message)) = receiver.next().await {
+                if let Message::Text(text) = message {
+                    let user_data = match Token::decode_token(text) {
+                        Ok(token) => token.claims.user,
+                        Err(_) => return Ok(()),
+                    };
+                    ws_state.register(&user_data.id, sink).await;
+                    break;
                 }
-            });
+            }
 
-            tokio::signal::ctrl_c().await.unwrap();
+            // Handle remaining messages
+            while let Some(Ok(message)) = receiver.next().await {
+                match message {
+                    Message::Text(text) => {
+                        println!("Received Text message: {}", text);
+                        let m: ChatMessage = serde_json::from_str(&text).unwrap_or_default();
+                        let receiver_id = Uuid::parse_str(&m.receiver_id).unwrap();
+                        let _ = ws_state.send(&receiver_id, WsMessage::Chat(m)).await;
+                    }
+                    Message::Close(_) => break,
+                    _ => {}
+                }
+            }
+
             Ok(())
         })
     })
